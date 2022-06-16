bab4068fc0534527cf565dbbea86e1cd93efe5e5
HoangHuy296 <19110218@student.hcmute.edu.vn>
2022-06-17 04:24:08 +0700

    cc

--- app/src/main/java/hcmute/spkt/mssv19110218/chatappzalo/Models/Contact.java
+++ app/src/main/java/hcmute/spkt/mssv19110218/chatappzalo/Models/Contact.java
@@ -2,28 +2,30 @@ package hcmute.spkt.mssv19110218.chatappzalo.Models;
 
 public class Contact {
 
-    private String name; //* Tên được lưu trong contact của phone
-    private String phoneNo; //* Số điện thoại được lưu trong contact của phone
+    private String name; //Tên được lưu trong danh bạ của phone
+    private String phoneNo; //số điện thoại được lưu trong contact của phone
 
     public String getPhoneNo() {
-        //* Khởi tạo biến phoneNoChange = phoneNo;
+        //khởi tạo biến phoneNoChange = phoneNo;
         String phoneNoChange = phoneNo;
-        //* Cắt bỏ kí tự đầu tiên của phoneNoChang (mặc định là số 0)
+        //cắt bỏ số 0 đầu và thêm subString để gán chuỗi +84 và sđt
         phoneNoChange = phoneNoChange.substring(1);
-        //* Lây chuỗi "+84" cộng với chuỗi phoneNoChange
         phoneNoChange = "+84" + phoneNoChange;
-        //Sử dụng replaceAll để xóa toàn bộ khoảng trấng trong phoneNo
+        //Sử dụng replaceAll để xóa toàn bộ khoảng trắng trong phoneNo
         return phoneNoChange.replaceAll("\\s+", "");
     }
 
+    //trả về tên
     public String getName() {
         return name;
     }
 
+    //gán lại tên
     public void setName(String name) {
         this.name = name;
     }
 
+    //gán lại phoneNo
     public void setPhoneNo(String phoneNo) {
         this.phoneNo = phoneNo;
     }
