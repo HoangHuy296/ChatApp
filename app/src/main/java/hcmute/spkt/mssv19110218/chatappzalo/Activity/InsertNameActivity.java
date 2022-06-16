f728d1798edb48fc57572e728bbf1c114bf84d0e
HoangHuy296 <19110218@student.hcmute.edu.vn>
2022-06-16 18:08:31 +0700

    cc

--- app/src/main/java/hcmute/spkt/mssv19110218/chatappzalo/Activity/InsertNameActivity.java
+++ app/src/main/java/hcmute/spkt/mssv19110218/chatappzalo/Activity/InsertNameActivity.java
@@ -12,28 +12,32 @@ import hcmute.spkt.mssv19110218.chatappzalo.databinding.ActivityInsertNameBindin
 
 public class InsertNameActivity extends AppCompatActivity {
 
-    ActivityInsertNameBinding binding;
+    ActivityInsertNameBinding binding; //Dùng để binding các view trong InsertNameActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityInsertNameBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        //Yêu cầu request vào editName
         binding.editName.requestFocus();
-
+        //set sự kiện onClick cho nút continueBtnName
         binding.continueBtnName.setOnClickListener(new View.OnClickListener() {
+            //hàm check cho continueBtnName
             @Override
             public void onClick(View view) {
+                //kiểm tra editName
                 if(!binding.editName.getText().toString().isEmpty()){
                     String name = binding.editName.getText().toString().trim();
                     if (name.isEmpty()) {
+                        //báo lỗi nếu empty
                         binding.editName.setError("Please type a name");
                         return;
                     }
+                    //hoàn thành thì nhảy vào insertPhone
                     Intent intent = new Intent(InsertNameActivity.this, InsertPhoneActivity.class);
-                    Common.currentUser = new User();
-                    Common.currentUser.setName(binding.editName.getText().toString());;
+                    Common.currentUser = new User(); //khởi tạo currentUser
+                    Common.currentUser.setName(binding.editName.getText().toString()); //set name cho currentUser
                     startActivity(intent);
                 }else{
 
